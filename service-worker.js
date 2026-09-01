c9ea9f27da9818c7ce8eca8f801944d536e4a4c0
veredg1-boop <veredg1@icloud.com>
2026-08-31 22:04:45 -0700

    Add service worker for installable app experience

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='monday-money-move-v1';
+const ASSETS=['./','./index.html','./privacy.html','./terms.html','./success.html','./manifest.webmanifest'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));});
