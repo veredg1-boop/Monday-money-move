7fc8f6f76289793a3cd9a16fec568736f74fbec2
veredg1-boop <veredg1@icloud.com>
2026-09-01 14:50:16 -0700

    Cache support and brand assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='monday-money-move-v3';
-const ASSETS=['./','./index.html','./privacy.html','./terms.html','./success.html','./manifest.webmanifest'];
+const CACHE='monday-money-move-v4';
+const ASSETS=['./','./index.html','./privacy.html','./terms.html','./support.html','./success.html','./manifest.webmanifest','./brand/app-icon-master.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));});
