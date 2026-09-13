615d178ace0d5e0e5b760bab238fec73aea4de20
veredg1-boop <veredg1@icloud.com>
2026-09-13 00:14:19 -0700

    Refresh client asset versions after membership activation

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='monday-money-move-membership-1';
-const ASSETS=['./','./index.html','./privacy.html','./terms.html','./support.html','./membership.js?v=paid-1','./conversion-preview.js?v=paid-1'];
+const CACHE='monday-money-move-membership-2';
+const ASSETS=['./','./index.html','./privacy.html','./terms.html','./support.html','./membership.js?v=paid-2','./conversion-preview.js?v=paid-2'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('monday-money-move-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
